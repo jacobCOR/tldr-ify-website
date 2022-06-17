448bf0624e725a86375c2920dea7692f1a89d024
jacobCOR <firelice@zenith>
2022-06-17 11:56:32 -0700

    [tweaks] update UI

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="footer">
   <div class="content has-text-centered">
-    <p>Made with ❤️ and ☕️ by Jacob</p>
+    <p>Made with ❤️ and ☕️ by Jacob and Jamie is big cutie</p>
   </div>
 </footer>
